6b9f1f3b9db08a94c3293d27520a13b591915e04
kubat <kubat.aiupov@gmail.com>
2023-04-01 15:10:05 +0200

    edit fetchData, edit renderData

--- app.js
+++ app.js
@@ -9,29 +9,39 @@ const outputEl = document.querySelector(".output");
 const API = "https://random-data-api.com/api/v2/banks";
 
 // fetched data
-let dataApi = [];
+//let dataApi = [];
 
 // fetch data
 const fetchData = async () => {
-  const response = await fetch(API);
+  try {
+    const response = await fetch(API);
   const data = await response.json();
-  dataApi.push(data);
-  console.log(dataApi);
-  renderData(dataApi);
+  //dataApi.push(data);
+  //console.log(data);
+  renderData(data);
+  } catch(error) {
+    console.log('Error occured: ', error)
+  }
+  
 };
 
 // display data
-const renderData = (d) => {
-  const renderItem = d.map((item) => {
+const renderData = (data) => {
+  //data = Object.keys(data);
+  console.log(data)
+  const renderItem = data.map((item) => 
     `<ul>
         <li>Bank Name: ${item.bank_name}</li>
         <li>ID: ${item.id}</li>
         <li>IBAN: ${item.iban}</li>
         <li>Account: ${item.account_number}</li>
-    </ul>`;
-  });
+    </ul>`)
+    .join('');
   outputEl.innerHTML = renderItem;
 };
 
-fetchData();
 
+btnEl.addEventListener('click', fetchData)
+
+//TODO
+//fix renderData map to array
